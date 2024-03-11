aced155b1c955bdbd8e3e920fe6db5010044cbea
Syed zadi <160313963+Syeda-Muneeba@users.noreply.github.com>
2024-03-11 21:51:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+//exercise 10
+//my name is Muneeeba
+//date 11-march -24
+console.log("hello world simple program");
---
+++ program2.js
@@ -0,0 +1,4 @@
+//exercise 10
+//my name is Muneeeba
+//date 11-march -24
+console.log(5 * 2);
